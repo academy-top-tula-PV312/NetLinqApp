b8404a0d66259c86fdc5dd03a8f8a0296f79e3a8
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-09-11 21:20:03 +0300

    Добавьте файлы проекта.

---
+++ NetLinqApp/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLinqApp
+{
+    public class Employee
+    {
+        static int globalId = 0;
+
+        public int Id { get; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee()
+        {
+            Id = ++globalId;
+        }
+    }
+}
---
+++ NetLinqApp/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLinqApp
+{
+    public static class ServiceApp
+    {
+        public static List<Employee> Init()
+        {
+            return new List<Employee>()
+            {
+                new(){ Name = "Bobby", Age = 25 },
+                new(){ Name = "Jimmy", Age = 31 },
+                new(){ Name = "Tommy", Age = 19 },
+            };
+        }
+    }
+}
